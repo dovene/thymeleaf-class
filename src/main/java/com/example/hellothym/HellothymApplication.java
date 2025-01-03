75530452a6a39763853fdfd2117e95eafe1f69e2
dovene <adovene@gmail.com>
2025-01-03 12:06:20 +0100

    first commit

---
+++ src/main/java/com/example/hellothym/HellothymApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellothym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellothymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellothymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellothym/HellothymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellothym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellothymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
